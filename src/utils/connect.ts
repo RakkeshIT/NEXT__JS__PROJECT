2f12ff8b5b61a542c01265071e4f400e1405ab5d
RakkeshSavael <156323140+RakkeshIT@users.noreply.github.com>
2025-11-18 14:15:44 +0530

    Update connect.ts

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,12 +1,12 @@
 import { MongoClient } from "mongodb";
 
 // Mongo DB URI Variable
-const URI = process.env.MongoDB_URI as string;
+const URI = process.env.MONGODB_URI as string;
 
 // Throw Error for MongoDB Connection is not Connect Properly
 if(!URI) throw new Error("Please Connect MongoDB Properly !!!!!!")
 
-    const globalWithMongo = global as typeof globalThis & {_mongoClient?: MongoClient};
+const globalWithMongo = global as typeof globalThis & {_mongoClient?: MongoClient};
     
 // Extend the global object to include _mongoClient for caching
 const client = globalWithMongo._mongoClient ?? new MongoClient(URI);
